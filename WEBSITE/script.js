b4cdb4ef0653d515a80291af12311f79604e9253
Abhijeet23-ui <abhijeetsarkate93@gmail.com>
2025-05-08 03:06:39 -0700

    Add files via upload

---
+++ WEBSITE/script.js
@@ -0,0 +1,17 @@
+// Scroll animation
+const sections = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
